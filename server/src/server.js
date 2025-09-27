e33c31f66b98dce8e1c0a900266efdfd1195cfd4
sujal2702 <sujalsingh2724@gmail.com>
2025-09-28 04:05:24 +0530

    coomit3

--- server/src/server.js
+++ server/src/server.js
@@ -1,10 +1,11 @@
+import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import app from './app.js';
 import { connectDB } from './config/db.js';
 
 dotenv.config();
 
-const PORT = process.env.PORT; // Render will provide the correct port
+const PORT = process.env.PORT;
 
 async function start() {
   try {
